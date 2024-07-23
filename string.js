4f6cc017094aed129ccabc1c082537d55c195822
Abhijit <abhijit.patra224@gmail.com>
2024-07-23 09:52:53 +0530

    string_session

---
+++ string.js
@@ -0,0 +1,34 @@
+let str ="abhi";
+//console.log(str.toUpperCase());
+
+let str1 = "CITY";
+//console.log(str1.toLowerCase());
+
+let str2 = "My name is is Abhi";
+let s1 = str2.replaceAll("is", "is not");
+//console.log(s1)
+
+let a = str2.split(" ", 0);
+//console.log(a);
+
+let str3 = "Facebook";
+
+//console.log(str3.substring(0,4));
+let str4 = "      My name is Sachin";
+
+//console.log(str4.trimStart());
+
+let str5 = "India";
+
+//console.log(str5.valueOf());
+
+
+let str6 = "I am learning javascript";
+
+console.log(str6.includes("javascrt"));
+
+
+
+
+
+
