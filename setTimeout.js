972172a5ffe1fbc490ac5cfc95202c5be691bfb9
Abhijit Patra <abhijit.patra224@gmail.com>
2024-09-07 02:35:18 +0530

    Settimeout

---
+++ setTimeout.js
@@ -0,0 +1,33 @@
+// function myName(m) {
+//     console.log("Hey"+ m," My name is Ganesha");
+// }
+
+// let t = setTimeout(myName, 1000, "Krishna");
+// clearTimeout(t);
+
+// function getName(m) {
+//     console.log("Hey "+ m +"My Name is Ganesha");
+// }
+
+// let i = setInterval(getName, 1000, "Krishna");
+
+// clearInterval(i);
+//let i =0;
+// function couter() {
+//     i++;
+// }
+
+// let i =0;
+// let timer = setTimeout(function myName(){
+//     i++;
+//     console.log(i);
+//     timer = setTimeout(myName, 1000);
+//     if(i ===5) {
+//         clearTimeout(timer);
+//     }
+// }, 1000);
+
+// setTimeout(()=> {
+//     console.log("My Name is Ganesha")
+// }, 0)
+
